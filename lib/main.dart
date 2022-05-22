2e684653b3badc32341dd09025db6809303db2f3
theemployee6-dev <theemployee6@gmail.com>
2022-05-22 19:33:49 -0300

    Main.dart
    iniciando o projet do zero, Álcool ou Gasolina

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [],
-      )),
+      home: Container(),
     );
   }
 }
